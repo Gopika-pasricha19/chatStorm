6a1708e7682adfd5d9955491c0efb4c379998980
Gopika1909 <119722625+Gopika1909@users.noreply.github.com>
2023-04-19 15:57:55 +0530

    commit

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
    });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
    console.log(`🎯 Server is running on PORT: ${PORT}`);
 });
